841b76060abb643d357f325d5df00634304220e8
TomJerry <499739027@qq.com>
2019-12-31 12:14:48 +0800

    删除垃圾

--- health_service/src/main/java/com/itheima/service/impl/RoleServiceImpl.java
+++ health_service/src/main/java/com/itheima/service/impl/RoleServiceImpl.java
@@ -11,7 +11,6 @@ import com.itheima.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 
 /**
  * @Author hzh
